036d181f7c1e0f6d41f9c9d7d1d968f1c7f862d5
Morgan Mueller <morgan.muellerb@gmail.com>
2020-04-18 13:57:26 -0400

    render updates

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,4 @@
-// Thank you to Sam Levine for this code!
+// Thank you to Sam Levine for the random array code!
 
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,5 +11,38 @@ bg_images.forEach((c) => {
     c.style.width = randint(250, 500) + 'px';
     c.style.display = 'block';
     c.classList.add('bg');
-  }, randint(1000, 10000));
+  }, randint(15000, 60000));
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+//slideshow
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
